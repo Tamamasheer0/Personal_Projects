749afe76a6ead968d8e2670a437482f8395d4158
RTamashiro <Tamashiroryan@gmail.com>
2019-07-01 20:01:02 -0700

    Delete obsolet/depricated code files from folder

--- Student_Mobility/Database_Setup/Archive/ConfigureMySQLDatabase.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine, engine, inspect
-from sqlalchemy import Column, Integer, String, Numeric
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
